63749f9ffe7fb40b81a1ced2e0011a832384e327
Tiana-Finaritra <hei.finaritra.3@gmail.Com>
2023-05-13 17:04:34 -0600

    feat: add user database

--- Back_Serveur/index.js
+++ Back_Serveur/index.js
@@ -16,18 +16,40 @@ const pool = new Pool({
     user: "postgres",
     host: "localhost",
     database: "finance",
-    password: "" ,//Your password postgresql
+    password: "12345678" ,//Your password postgresql
     port: 5432 // default port on postgresql
   });
   console.log("Connexion réussie à la base de données");
 
-  
-
-app.listen(3000, () => {
-  console.log("Serveur démarré (http://localhost:3000/) !");
+app.listen(8000, () => {
+  console.log("Serveur démarré (http://localhost:8000/) !");
 });
+
+
+
 
 app.get("/", (req, res) => {
-  // res.send("Bonjour le monde...");
-  res.render("index");
-});
+  const query = `
+    SELECT
+      (SELECT COUNT(*) FROM "user") AS count_user,
+      *
+    FROM
+      "user"
+    ORDER BY
+      id_user
+  `;
+
+  pool.query(query, [], (err, result) => {
+    if (err) {
+      console.error(err.message);
+      return res.status(500).send('Erreur de serveur');
+    }
+    
+      res.render("index", 
+      { 
+        countUser: result.rows[0].count_user,
+        user: result.rows
+      });
+    });
+  });
+
