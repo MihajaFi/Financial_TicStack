8bd280d42d903fd5471b1481ee4201de9adb20ab
Fifaliana <hei.fifaliana@gmail.com>
2023-06-21 10:19:21 +0300

    feat add information

--- Back_Serveur/public/js/main.js
+++ Back_Serveur/public/js/main.js
@@ -25,3 +25,82 @@ const showMenu = (toggleId, navbarId) =>{
     }
 }
 showMenu('nav-toggle','nav')
+
+// Add modal
+
+const Modal = {
+    open(){
+        //Ouvrir la fenêtre modale
+     // Ajouter la classe active à la fenêtre modale
+        document
+            .querySelector('.modal-overlay')
+            .classList
+            .add('active')
+
+    },
+    close(){
+        // fermer le module 
+        // supprime la classe active a la fenetre module
+        document
+            .querySelector('.modal-overlay')
+            .classList
+            .remove('active')
+    }
+}
+const Form = {
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+
+    getValues() {
+        return {
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value
+        }
+    },
+
+    validateFields() {
+        const { description, amount, date } = Form.getValues()
+        
+        if( description.trim() === "" || 
+            amount.trim() === "" || 
+            date.trim() === "" ) {
+                throw new Error("Veuillez remplir ce fiche ")
+        }
+    },
+
+    formatValues() {
+        let { description, amount, date } = Form.getValues()
+        
+        amount = Utils.formatAmount(amount)
+
+        date = Utils.formatDate(date)
+
+        return {
+            description,
+            amount,
+            date
+        }
+    },
+
+    clearFields() {
+        Form.description.value = ""
+        Form.amount.value = ""
+        Form.date.value = ""
+    },
+
+    submit(event) {
+        event.preventDefault()
+
+        try {
+            Form.validateFields()
+            const transaction = Form.formatValues()
+            Transaction.add(transaction)
+            Form.clearFields()
+            Modal.close()
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+}
