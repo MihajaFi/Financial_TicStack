356408b5b12ba8b703000c8145d15c6a2b45a3a7
Ricka Princy <127594276+RickaPrincy@users.noreply.github.com>
2023-06-21 14:46:42 +0300

    feat: make getRacine multiplateforme (#20)

--- Back_Serveur/src/utils.js
+++ Back_Serveur/src/utils.js
@@ -5,6 +5,5 @@ const __dirname = path.dirname(__filename);
 
 
 export function getRacine(){
-    const index = __dirname.indexOf("/src");
-    return __dirname.slice(0,index);
+    return __dirname.slice(0,__dirname.length - 4);
 }
