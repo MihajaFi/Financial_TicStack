d88c3649fad7a3bff2d50f1a1b82c08e5a858a79
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2023-07-07 11:09:48 +0300

    update MDC

--- Database/Implementation.sql
+++ Database/Implementation.sql
@@ -1,9 +1,14 @@
-\c postgres;
+\ c postgres;
+
 DROP DATABASE IF EXISTS finance;
+
 CREATE DATABASE finance;
-\c finance;
+
+\ c finance;
+
 -- "user"
 DROP TABLE IF EXISTS "user";
+
 CREATE TABLE "user"(
   id_user SERIAL PRIMARY KEY,
   profil VARCHAR(300),
@@ -19,15 +24,17 @@ CREATE TABLE "user"(
   email TEXT
 );
 
-
 -- group_account
 DROP TABLE IF EXISTS group_account;
+
 CREATE TABLE group_account(
   id_account SERIAL PRIMARY KEY,
   total_money FLOAT NOT NULL
 );
+
 -- pay 
 DROP TABLE IF EXISTS pay;
+
 CREATE TABLE pay(
   id SERIAL PRIMARY KEY,
   "value" FLOAT NOT NULL,
@@ -36,20 +43,14 @@ CREATE TABLE pay(
   week Date,
   id_user SERIAL REFERENCES "user"(id_user)
 );
+
 -- withdrawal
 DROP TABLE IF EXISTS withdrawal;
+
 CREATE TABLE withdrawal(
   id SERIAL PRIMARY KEY,
   "value" FLOAT NOT NULL,
   withdrawal_date TimeStamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   reason Varchar(250),
   id_user SERIAL REFERENCES "user"(id_user)
-);
-
-
-
-CREATE TABLE contain (
-  quantity INT NOT NULL ,
-  order_id INT REFERENCES order(id_order) ,
-  article_id INT REFERENCES article(id_article)
 );
